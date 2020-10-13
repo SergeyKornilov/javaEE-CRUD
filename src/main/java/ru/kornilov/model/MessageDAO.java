e29e01e0e82043e7dadf7c667562c72cf664b65d
SergeyKornilov <kornilovsergey.box@gmail.com>
2020-10-13 23:29:33 +0300

    add MessageDAO - SQL Query and conf

--- src/main/java/ru/kornilov/model/MessageDAO.java
+++ src/main/java/ru/kornilov/model/MessageDAO.java
@@ -1,7 +1,13 @@
 package ru.kornilov.model;
 
+import ru.kornilov.entities.Message;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MessageDAO {
-    private String jdbcURL = "jdbc:postgresql://localhost:8080/JavaECrud";
+    private String jdbcURL = "jdbc:postgresql://localhost:5432/JavaECrud";
     private String name = "root";
     private String password = "1234";
 
@@ -16,6 +22,46 @@ public class MessageDAO {
     private static final String SELECT_ALL_MESSAGES_SQL = "SELECT * FROM messages";
     private static final String DELETE_MESSAGES_SQL = "DELETE FROM messages WHERE id = ?";
     private static final String UPDATE_MESSAGE_SQL = "UPDATE users SET author = ?, text = ?  WHERE id = ?";
+
+    protected Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(jdbcURL, name, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+    public void insertMessage(Message message) {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_MESSAGE_SQL)) {
+            preparedStatement.setString(1, message.getAuthor());
+            preparedStatement.setString(2, message.getText());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Message> selectAllMessage() throws SQLException {
+        List<Message> messages = new ArrayList<>();
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_MESSAGES_SQL)) {
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String author = rs.getString("author");
+                String text = rs.getString("text");
+                messages.add(new Message(id, author, text));
+            }
+        }
+        return messages;
+    }
 
 
 
